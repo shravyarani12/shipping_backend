8c7329ec8842d91f19692b953a43a759aa206b94
Shravya rani <shravyarani.gre@gmail.com>
2022-06-16 01:26:19 -0400

    done

---
+++ jwt.js
@@ -0,0 +1,26 @@
+
+//const jwt = require('jsonwebtoken');
+
+import jwt from "jsonwebtoken";
+
+
+
+export function generateAccessToken(username,callback) {
+    jwt.sign({user:username}, process.env.TOKEN_SECRET, {expiresIn: '7d'},(err,token)=>{
+        return callback(err,token)
+    })
+}
+
+export function authenticateToken(req, res, next,callback) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    console.log(token)
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, detoken) => {
+        let resp={
+            user:detoken.user
+        }
+        return callback(err,resp)
+    })
+}
